b9699bbe2854b423bc68e70353fd8d562494d329
josh Zhang <josh.zhang@c3iot.com>
2019-10-28 15:21:53 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="text_detector",
+    version="0.0.1",
+    author="Josh Zhang",
+    description="Merging EAST, CRAFT and CTPM",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
